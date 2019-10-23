deabca6a4beffe86f1bbca550e0ed3ac70c519b7
Gabe Amaya <gabeamaya2@gmail.com>
2019-10-23 13:45:31 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,8 +59,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-108965203-2`,
+        head: true,
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
       },
     },
     {
